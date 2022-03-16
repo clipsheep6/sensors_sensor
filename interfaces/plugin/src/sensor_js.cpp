381c455fca068570d39e83833676921a16fd2792
cff-gite <chenfeifei8@huawei.com>
2022-03-16 16:21:33 +0800

    修改sensor代码
    Signed-off-by: cff-gite <chenfeifei8@huawei.com>

--- interfaces/plugin/src/sensor_js.cpp
+++ interfaces/plugin/src/sensor_js.cpp
@@ -256,7 +256,11 @@ static uint32_t RemoveCallback(napi_env env, int32_t sensorTypeId, napi_value ca
 {
     std::vector<struct AsyncCallbackInfo*> callbackInfos = g_onCallbackInfos[sensorTypeId];
     std::vector<struct AsyncCallbackInfo*>::iterator iter;
-    for (iter = callbackInfos.begin(); iter != callbackInfos.end(); iter++) {
+    for (iter = callbackInfos.begin(); iter != callbackInfos.end();) {
+        if (*iter == nullptr || (*iter)->callback[0] == nullptr) {
+            HiLog::Error(LABEL, "%{public}s arg is null", __func__);
+            continue;
+        }
         napi_value sensorCallback = nullptr;
         napi_get_reference_value(env, (*iter)->callback[0], &sensorCallback);
         if (IsNapiValueSame(env, callback, sensorCallback)) {
@@ -264,11 +268,13 @@ static uint32_t RemoveCallback(napi_env env, int32_t sensorTypeId, napi_value ca
             (*iter)->callback[0] = nullptr;
             delete *iter;
             *iter = nullptr;
-            callbackInfos.erase(iter);
+            iter = callbackInfos.erase(iter);
             if (callbackInfos.empty()) {
                 g_onCallbackInfos.erase(sensorTypeId);
                 return 0;
             }
+        } else {
+            ++iter;
         }
     }
     g_onCallbackInfos[sensorTypeId] = callbackInfos;
